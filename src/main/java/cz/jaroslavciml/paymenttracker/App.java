071b56ccb00f8de965ab55f3ddf9bb9973034536
Jaroslav Ciml <cimlj@centrum.cz>
2016-06-17 15:32:07 +0200

    Create initial project structure

---
+++ src/main/java/cz/jaroslavciml/paymenttracker/App.java
@@ -0,0 +1,8 @@
+package cz.jaroslavciml.paymenttracker;
+
+public class App {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");//TODO JC just to test initial app build
+    }
+}
