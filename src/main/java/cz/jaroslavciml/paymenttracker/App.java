382dce7f323ea412dbb66e2fda9c0efb9c9b463e
Jaroslav Ciml <cimlj@centrum.cz>
2016-06-20 10:03:49 +0200

    Support input file

--- src/main/java/cz/jaroslavciml/paymenttracker/App.java
+++ src/main/java/cz/jaroslavciml/paymenttracker/App.java
@@ -3,6 +3,8 @@ package cz.jaroslavciml.paymenttracker;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -11,28 +13,59 @@ public class App {
     private static final PaymentSummarizer paymentSummarizer = new PaymentSummarizer();
 
     public static void main(final String[] args) {
+        if (args.length > 1) {
+            System.err.println("Supported arguments: [input_filename]");
+            System.exit(1);
+        }
+        final String inputFilename = args.length > 0 ? args[0] : null;
+
+        if (inputFilename != null) {
+            readFile(inputFilename);
+        }
         final PrintThread printThread = new PrintThread(paymentSummarizer);
         printThread.start();
         try {
-            readInput();
+            readStandardInput();
         } finally {
             printThread.die();
         }
     }
 
-    private static void readInput() {
-        try (final BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in))) {
-            String line;
-            while ((line = inputReader.readLine()) != null)
-            {
-                if (StringUtils.strip(line).equalsIgnoreCase("quit")) {
-                    break;
-                }
-                processPaymentLine(line);
-            }
+    private static void readFile(final String filename) {
+        final FileReader fileReader;
+        try {
+            fileReader = new FileReader(filename);
+        } catch (final FileNotFoundException e) {
+            System.err.println("Cannot open '" + filename + "'");
+            System.exit(1);
+            throw new IllegalStateException("This point should never be reached"); // just to prevent compiler error
+        }
+
+        try (final BufferedReader reader = new BufferedReader(fileReader)) {
+            readInput(reader, false);
         } catch (final IOException e) {
             System.err.println("I/O error occurred when reading from standard input");
             System.exit(1);
+        }
+    }
+
+    private static void readStandardInput() {
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            readInput(reader, true);
+        } catch (final IOException e) {
+            System.err.println("I/O error occurred when reading from standard input");
+            System.exit(1);
+        }
+    }
+
+    private static void readInput(final BufferedReader reader, final boolean supportQuit) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null)
+        {
+            if (supportQuit && StringUtils.strip(line).equalsIgnoreCase("quit")) {
+                break;
+            }
+            processPaymentLine(line);
         }
     }
 
